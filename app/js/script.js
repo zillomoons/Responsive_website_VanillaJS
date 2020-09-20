0d774516f504432ed3d25ef0df1d81267f57340e
zillomoons <dlzht73@ya.ru>
2020-09-20 22:53:23 +0300

    added keyframes

--- app/js/script.js
+++ app/js/script.js
@@ -1,13 +1,18 @@
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
 
 btnHamburger.addEventListener('click', function() {
     console.log('open hamburger');
     
     if(header.classList.contains('open')){
         header.classList.remove('open');
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
     }
-    else{
+    else{ // open hamburger
         header.classList.add('open');
+        overlay.classList.remove('fade-out');
+        overlay.classList.add('fade-in');
     }
 })
