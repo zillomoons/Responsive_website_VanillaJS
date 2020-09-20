b3d2d89169fb3c10c1b40069892a6b6219006a5f
zillomoons <dlzht73@ya.ru>
2020-09-20 20:07:44 +0300

    hamburger menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,13 @@
-console.log('hello world')
+const btnHamburger = document.querySelector('#btnHamburger');
+
+btnHamburger.addEventListener('click', function() {
+    console.log('open hamburger');
+    
+    if(btnHamburger.classList.contains('open')){
+        btnHamburger.classList.remove('open');
+    }
+    else{
+        btnHamburger.classList.add('open');
+    }
+    
+})
