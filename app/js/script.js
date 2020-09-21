cc236d783752423ce80a75cec40826553d032120
zillomoons <dlzht73@ya.ru>
2020-09-21 12:26:36 +0300

    added mobile menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,18 +1,25 @@
 const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
 const header = document.querySelector('.header');
-const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
 
 btnHamburger.addEventListener('click', function() {
     console.log('open hamburger');
     
     if(header.classList.contains('open')){
+        body.classList.remove('noscroll');
         header.classList.remove('open');
-        overlay.classList.remove('fade-in');
-        overlay.classList.add('fade-out');
+        fadeElems.forEach(function(elem){
+            elem.classList.remove('fade-in');
+            elem.classList.add('fade-out');
+        })
     }
     else{ // open hamburger
+        body.classList.add('noscroll');
         header.classList.add('open');
-        overlay.classList.remove('fade-out');
-        overlay.classList.add('fade-in');
+        fadeElems.forEach(function(elem){
+            elem.classList.remove('fade-out');
+            elem.classList.add('fade-in');
+        })
     }
 })
